080eb68f9fdc2cb17087204560763d382d8f874b
xp-squared <zaneimportant@gmail.com>
2024-07-25 19:58:46 -0400

    Finished

--- Algorithms/C++/MergeSort.cpp
+++ Algorithms/C++/MergeSort.cpp
@@ -13,14 +13,46 @@ void displayVector(vector<Type>& vec) {
 
 template <typename Type>
 void merge(vector<Type>& vec, int left, int right, int middle) {
+    vector<Type> tempVec;
     
+    int i = left; // starting index for left half
+    int j = middle + 1; // starting index for right half
+
+    // comparing and copying with the indexes
+    while (i <= middle && j <= right) {
+        if(vec[i] <= vec[j]) {
+            tempVec.push_back(vec[i]);
+            i++;
+        }
+        else {
+            tempVec.push_back(vec[j]);
+            j++;
+        }
+    }
+
+    // now we have to make sure all data is inserted
+    while (i <= middle) {
+        tempVec.push_back(vec[i]);
+        i++;
+    }
+    while (j <= right) {
+        tempVec.push_back(vec[j]);
+        j++;
+    }
+
+    // now copy over elements
+    // recursion is why we used the left starting index
+    // if we had just started from index 0 instead we would overwrite elements not belonging to the subarray we are currently in 
+    for(int z = 0; z < tempVec.size(); z++) {
+        vec[left + z] = tempVec[z];
+    }
 }
 
 template <typename Type>
 void mergeSort(vector<Type>& vec, int left, int right) {
     // this is where we are going to break up everything with recursion
 
-    // if we have reached the smallest possible list
+    // if we have reached the smallest possible list, left index is greater than right
     if (left >= right) {
         return;
     }
@@ -47,6 +79,7 @@ int main() {
     mergeSort(vec1, 0, vec1.size() - 1);
 
     cout << "\nList after sorting: ";
+    displayVector(vec1);
 
     return 0;      
 }
