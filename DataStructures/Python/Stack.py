8bf2a3c883911e8aa5f250d46021c10c3bb6f1db
xp-squared <zaneimportant@gmail.com>
2024-07-26 12:42:32 -0400

    Stack finished

--- DataStructures/Python/Stack.py
+++ DataStructures/Python/Stack.py
@@ -12,17 +12,28 @@ class Stack:
     def isEmpty(self):
         return len(self.stackList) == 0
     
+    # appending an element to the front of stack, this becomes the new head of it
     def push(self, data):
         self.stackList.append(data)
     
+    # function to remove the top most element of the stack
     def pop(self):
         if self.isEmpty() == True:
             # throwing exception for the stack being empty
             return print("Popping from empty list")
         else:
             # removes item at the last index, which is the default of pop method
+            # we can display which element we are removing by using our peek function
+            print("\nWe are popping: ", self.peek())
+            print()
             return self.stackList.pop() 
 
+    # function to peek at what is at the top of the stack 
+    def peek(self):
+        if self.isEmpty == 0:
+            print("Size is 0, cannot peek")
+        else:
+            return self.stackList[-1]
 
     def size(self):
         return len(self.stackList)
@@ -31,11 +42,11 @@ class Stack:
         if self.isEmpty() == True:
             print("Empty List, nothing to print")
             return
-        
-        print()
+        print("Current List: ", end = " ")
         for i in range(len(self.stackList)):
             # printing each element, remember we can make the print function end with a space instead of how it ends always with a new line
             print(self.stackList[i], end = " ")
+        print()
 
         
 
@@ -47,5 +58,14 @@ def main():
     s.display()
     s.pop()
     s.display()
+    s.push(25)
+    s.push(69)
+    s.display()
+    s.pop()
+    s.display()
+
+    # enable it where a user can insert and pop to do this for simplicity
+    # fix spacing issue with display
+
 
 main()
