1bb1daeced70e93b48fdbd7ddc2a28425f92ad1c
xp-squared <zaneimportant@gmail.com>
2025-01-21 13:13:11 -0500

    Small changes

--- Practice/SameCircumference.py
+++ Practice/SameCircumference.py
@@ -16,15 +16,23 @@ def main():
     #    print(point)
 
     # now lets check if the points entered lie on the circumference
-    print("Checking if points lie on the circumference")
+    print("Checking if points lie on the circumference\n")
+    flag = True
     for i, (x,y) in enumerate(points):
         distance = math.sqrt(((x - h)**2 + (y - k)**2))
         if (distance < radius):
-            print(f"point {i+1} with coords ({x}, {y}) is not on the circumference (inside)")
+            print(f"point {i+1} with coords ({x}, {y}) is not on the circumference (inside)\n")
+            flag = False
         elif (distance > radius):
-            print(f"point {i+1} with coords ({x}, {y}) is not on the circumference (outside)")
-        else:
-            print(f"point {i+1} with coords ({x}, {y}) is on the circumference!")
+            print(f"point {i+1} with coords ({x}, {y}) is not on the circumference (outside)\n")
+            flag = False
+        else: # this could be improved because ofpossible floating point errors (abs(distance - radius) < 1e-9)
+            print(f"point {i+1} with coords ({x}, {y}) is on the circumference!\n")
+    
+    if flag == False:
+        print("Not all the points lie on the same circumference")
+    else:
+        print("All points lie on the same circumference")
     
 
 
